3e344b979b3f00ed51f947799811288615ea1a4e
Naresh-1308 <raghavgupta227@gmail.com>
2023-08-15 01:08:06 +0530

    naresh

--- js/main.js
+++ js/main.js
@@ -62,11 +62,11 @@
         loop: true,
         center: true,
         dots: false,
-        nav: true,
+        /*nav: true,
         navText : [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
-        ],
+        ],*/
         responsive: {
 			0:{
                 items:1
